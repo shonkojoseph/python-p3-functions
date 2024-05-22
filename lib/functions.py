0215ef459182374b4fd9290c2fc129c0875a6930
Joseph <josephshonko99@gmail.com>
2024-05-22 08:54:12 +0300

    commiting p3- functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
+
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+
+greet_programmer()              
+greet("Hello")                  
+greet_with_default()            
+greet_with_default("programer")       
+print(add(3, 4))                
+print(halve(10))                
